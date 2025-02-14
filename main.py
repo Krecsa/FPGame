c53fc5cfa62366e267735cccad3efdd16032f71d
Krecsa <malistova02@mail.ru>
2025-02-14 15:43:14 +0300

    Фон + персонаж с анимацией ходьбы

--- main.py
+++ main.py
@@ -1,15 +1,53 @@
 import pygame
 
+clock = pygame.time.Clock()
+
 pygame.init()
-screen = pygame.display.set_mode((600, 300))
+screen = pygame.display.set_mode((900, 500))
 pygame.display.set_caption('FPGame')
 icon = pygame.image.load('images/icon.png')
 pygame.display.set_icon(icon)
 
+bg = pygame.image.load('images/background.jpg')
 
+walk_right = [
+    pygame.image.load('images/w_right/r1.png'),
+    pygame.image.load('images/w_right/r2.png'),
+    pygame.image.load('images/w_right/r3.png'),
+    pygame.image.load('images/w_right/r4.png'),
+    pygame.image.load('images/w_right/r5.png'),
+    pygame.image.load('images/w_right/r6.png'),
+    pygame.image.load('images/w_right/r7.png'),
+    pygame.image.load('images/w_right/r8.png')
+]
+walk_left = [
+    pygame.image.load('images/w_left/l1.png'),
+    pygame.image.load('images/w_left/l2.png'),
+    pygame.image.load('images/w_left/l3.png'),
+    pygame.image.load('images/w_left/l5.png'),
+    pygame.image.load('images/w_left/l6.png'),
+    pygame.image.load('images/w_left/l7.png'),
+    pygame.image.load('images/w_left/l8.png')
+]
+
+wolf_anim_count = 0
+bg_x = 0
 
 running = True
 while running:
+    clock.tick(10)
+    screen.blit(bg, (bg_x,0))
+    screen.blit(bg, (bg_x + 900, 0))
+    screen.blit(walk_right[wolf_anim_count], (300, 325))
+
+    if wolf_anim_count == 7:
+        wolf_anim_count = 0
+    else:
+        wolf_anim_count += 1
+
+    bg_x -= 2
+    if bg_x == - 900:
+        bg_x == 0
 
     pygame.display.update()
 
@@ -17,3 +55,4 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             pygame.quit()
+
